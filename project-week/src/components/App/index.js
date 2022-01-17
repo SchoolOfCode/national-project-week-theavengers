e9827ba7d50ed24a9b4ffee4efb3b25f5521eeaf
Sareena <sareena.naser@ibm.com>
2022-01-17 14:52:29 +0000

    setting state for quote

--- project-week/src/components/App/index.js
+++ project-week/src/components/App/index.js
@@ -1,7 +1,7 @@
 import {Button, Container, Col, Row} from 'react-bootstrap'
 
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState, useRef} from "react";
 
 //const api_url ="https://zenquotes.io/api/quotes/";
 // async function getapi(url)
@@ -18,15 +18,25 @@ import {useEffect} from "react";
 
 function App() {
 
+  const [quote, setQuote] = useState("")
+  const latestQuote = useRef(quote)
+
+
   useEffect(()=>{
     async function getQuoteOfTheDay(){
-      const response = await fetch("https://zenquotes.io/api/quotes/")
-      console.log(response)
+      const response = await fetch("https://inspiring-quotes.p.rapidapi.com/random?author=Albert", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "inspiring-quotes.p.rapidapi.com",
+          "x-rapidapi-key": "66e15a0c63msh64f1ed7a108ca24p11da89jsn1c2cd45ce893"
+        }})
       const data = await response.json();
-      console.log(data)
+      setQuote(latestQuote.current = data.quote)
+      return data
     }
     getQuoteOfTheDay()
-  })
+  }, [])
+  
   return (
     <div className="App">
       

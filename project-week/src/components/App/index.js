a15f7fa926aebccf42997be91b6d0df522e47b2e
The-Six <ijazdriscoll@gmail.com>
2022-01-17 15:17:16 +0000

    nav-bar completed

--- project-week/src/components/App/index.js
+++ project-week/src/components/App/index.js
@@ -1,4 +1,6 @@
 import {Button, Container, Col, Row} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
 
 
 import './App.css';
@@ -7,6 +9,16 @@ function App() {
 
   return (
     <div className="App">
+    
+      <nav>
+        <Link to="/">home</Link>
+        <br/>
+        <Link to="/Reflections">reflections</Link>
+        <br/>  
+        <Button>Create</Button>
+      </nav>
+
+      
       
     </div>
   );

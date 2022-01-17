2fe72dd1c6aca81653ccabac385ab68e3e686c3d
Sareena <sareena.naser@ibm.com>
2022-01-17 12:35:46 +0000

    bootstrap

---
+++ project-week/src/components/App/index.js
@@ -0,0 +1,15 @@
+import {Button, Container, Col, Row} from 'react-bootstrap'
+
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;

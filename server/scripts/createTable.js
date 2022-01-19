9d388c7d6c4b3dad5ff97466e19a3e3c6e8aee69
Sareena <sareena.naser@ibm.com>
2022-01-19 10:55:12 +0000

    pg setup and database created with pool and .env

--- server/scripts/createTable.js
+++ server/scripts/createTable.js
@@ -1,1 +1,11 @@
-// Todo
+
+import query from "../db/db.js"
+
+const sqlString = `CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, title TEXT, author TEXT)`;
+
+async function createBooksTable() {
+  const res = await query(sqlString);
+  console.log("Created books table", res);
+}
+
+createBooksTable();

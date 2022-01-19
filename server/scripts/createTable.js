a92e2f3d653725060d48fcc059ef360f89206040
Sareena <sareena.naser@ibm.com>
2022-01-19 12:06:40 +0000

    createTable

--- server/scripts/createTable.js
+++ server/scripts/createTable.js
@@ -1,1 +1,12 @@
 // Todo
+
+import query from "../db/db.js";
+
+const sqlString = `CREATE TABLE IF NOT EXISTS PostTable (id SERIAL PRIMARY KEY, text TEXT, timestamp TEXT)`;
+
+async function createPostsTable() {
+  const res = await query(sqlString);
+  console.log("Created posts table", res);
+}
+
+createPostsTable();

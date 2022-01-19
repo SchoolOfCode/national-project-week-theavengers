088a1ae24f9da2ddb5d8cbb7f0b9056aee58f555
“ilhamabdi” <ilhamabdi13@gmail.com>
2022-01-19 11:52:08 +0000

    Model Posts

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,25 +1,33 @@
-
-
+import query from "../db"
+//
 // Put database queries here........... 
 
 // Get all posts
-export function getPosts(){
-
+export async function getPosts(){
+    const data = query(`SELECT * FROM PostTable` );
+    return data.row;
 }
 
 // Get post by ID
-export function getPostById(){
-
+export async function getPostById(requestId){
+    const data = await query(`SELECT * FROM WHERE id=$1 ,[requestId]`)
+    return data.row;
 }
 
 //Create post..
-export function createPost(){
-
+export async function createPost(post){
+    const data = await query(`INSERT INTO PostTable(text,timestamp) VALUES ($1, $2), [post.text, post.timestamp]`)
+    return data.row;
 }
 
-export function deletePost(){
-
+export async function deletePostById(requestId){
+    const data = await query(`DELETE FROM PostTable WHERE requestId=$1`, [requestId]);
+    return data.row;
 }
 
 //Update post..
-//Replace post..
+    export async function updatePostById(requestId, newcard){
+        const data = await query(`UPDATE PostTable SET text= $1, timestamp=$2 WHERE id=requestId, [newcard.text,newcard.timestamp]`)
+        return data.row
+    }
+

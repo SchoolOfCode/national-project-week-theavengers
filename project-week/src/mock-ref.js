1ed9f37962599853dd4b881c5d3e03542a2b7dec
“ilhamabdi” <ilhamabdi13@gmail.com>
2022-01-21 11:22:55 +0000

    times

--- project-week/src/mock-ref.js
+++ project-week/src/mock-ref.js
@@ -1,17 +1,17 @@
 const reflectiions = [{
     id: 0,
     text: "“Today was tough. It was hard and I struggled so much that for the first time in this course I thought about quitting. I haven’t told anyone. I don’t think I can but I wanted to leave the confession somewhere.",
-    timestamp: "12/02/12"
+    timestamp: "17:45 10/02/12"
 },
 {
     id: 1,
     text: "Things went a little better. A little. Not a whole lot but enough that I felt a little lighter.",
-    timestamp: "12/02/12"
+    timestamp: "19:51 11/02/12"
 },
 {
     id: 2,
     text:"I spoke in class today. As in, I actually answered a question and helped one of my classmates. I know what it feels like to be standing in front of people so I had to help.",
-    timestamp: "12/02/12"
+    timestamp: "00:01 12/02/12"
 }]
 
 export default reflectiions
